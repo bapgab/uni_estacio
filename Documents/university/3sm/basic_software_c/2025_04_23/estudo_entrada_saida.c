af039f5ae36bfeb9776ebee00d30d478762897f3
gabrielbaptista <gabrielbaptistams@gmail.com>
2025-04-24 08:50:29 -0300

    atualização em programação de software básico em c

---
+++ Documents/university/3sm/basic_software_c/2025_04_23/estudo_entrada_saida.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+//padrão. scanf, getchar, printf, puts
+#include <curses.h>
+//getch (oculta o que o user digita),  
+#include <stdlib.h>
+
+/*
+// *** PRIMEIRO EX ***
+
+int main(int argc, char *argv[]){
+  char nome[15];
+  char sobrenome[15];
+  //define a var nome [vetor] e sobrenome [vetor]
+
+  printf("insira o nome\n");
+  scanf(" %s", nome);
+  getchar();
+  //getchar para limpar a memória depois do enter
+
+  printf("insira o sobrenome\n");
+  fgets(sobrenome, 15, stdin);
+  
+  printf("%s %s\n", nome, sobrenome);
+
+  return EXIT_SUCCESS;
+}
+*/
+
+// *** SEGUNDO EX ***
+
+int main(int argc, char *argv[]){
+
+
+
+
+  return EXIT_SUCCESS;
+}
